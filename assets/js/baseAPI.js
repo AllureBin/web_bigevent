10ed2f54fbe833ad4632047cff095adebe658ef6
liaolibin <643861889@qq.com>
2020-09-25 20:53:15 +0800

    完成主页功能模块

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,6 +5,25 @@ $(function() {
     $.ajaxPrefilter(function(options) {
         // 发起ajax请求之前，拼接请求的根路径
         options.url = 'http://ajax.frontend.itheima.net' + options.url
-        console.log(options.url);
+
+        // 统一给有权限的接口设置herders请求头
+        if (options.url.indexOf('/my/') !== -1) {
+            options.headers = {
+                Authorization: localStorage.getItem('token') || ''
+            }
+        }
+
+        // 全局统一挂载complete函数
+        // 无论请求成功还是失败，都会调用的函数
+        options.complete = function(res) {
+            // console.log(res);
+            // 在complete函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                // 强制清空token
+                localStorage.removeItem('token')
+                    // 强制跳转到登录页面
+                location.href = '/login.html'
+            }
+        }
     })
 })
---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+$(function() {
+    getUSerInfo()
+
+    var layer = layui.layer
+
+    // 点击退出事件
+    $('#logout').on('click', function() {
+        layer.confirm('是否确定退出登录？', { icon: 3, title: '提示' }, function(index) {
+            // 清除本地存储
+            localStorage.removeItem('token')
+                // 跳转回登录页面
+            location.href = '/login.html'
+                // 关闭confirm询问框
+            layer.close(index);
+        });
+    })
+})
+
+// 获取用户的信息
+function getUSerInfo() {
+    $.ajax({
+        mothod: "GET",
+        url: "/my/userinfo",
+        // headers就是请求头配置对象
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg("获取用户信息失败！")
+            }
+            // 调用渲染用户头像
+            renderAvatar(res.data)
+        }
+    })
+}
+
+// 渲染用户头像
+function renderAvatar(user) {
+    // 获取用户昵称
+    var name = user.nickname || user.username
+        // 设置欢迎文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+        // 渲染用户头像
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
